3447f0e6ee6a51494454e01d9dfeb453c3cca5a4
George Spiridonov <173371125+georgij-spiridonov@users.noreply.github.com>
2026-03-01 13:06:39 +0400

    Получение доступных языков автоматизировано

--- app/components/DashboardNavbar.vue
+++ app/components/DashboardNavbar.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import { en, ru } from "@nuxt/ui/locale";
+import * as locales from "@nuxt/ui/locale";
 
-const { locale: i18nLocale, setLocale } = useI18n();
+const { locale: i18nLocale, locales: i18nLocales, setLocale } = useI18n();
 
 const currentLocale = computed({
   get: () => i18nLocale.value,
   set: (val) => {
     setLocale(val);
   },
+});
+
+const availableLocales = computed(() => {
+  return i18nLocales.value.map((l) => (locales as any)[l.code]);
 });
 </script>
 
@@ -21,7 +25,7 @@ const currentLocale = computed({
     </template>
 
     <template #right>
-      <ULocaleSelect v-model="currentLocale" :locales="[en, ru]" />
+      <ULocaleSelect v-model="currentLocale" :locales="availableLocales" />
       <UColorModeButton />
 
       <UButton
